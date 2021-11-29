beb177e6237b2d91e39540edfaf7efca5ef4c572
Miguel Pacheco <minipachru@gmail.com>
2021-11-29 09:39:02 -0300

    update value of var on conditionaL

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
     var task2 = true;
   
     if (trueOrFalse) {
-      var task = true;
-      var task2 = false;
+      var task = false;
+      var task2 = true;
     }
   
     return [task, task2];
