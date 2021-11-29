d1e08d06e17506c7b74a014d334a2394a9180e16
Miguel Pacheco <minipachru@gmail.com>
2021-11-29 19:18:56 -0300

    Change conditional

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    task;
-    task2;
+    const task = true;
+    const task2 = false
   }
 
   return [task, task2];
