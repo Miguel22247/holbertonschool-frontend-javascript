cce090754d476ee958a555481b08aac9729e70cf
Miguel Pacheco <minipachru@gmail.com>
2021-11-29 09:46:09 -0300

    fix eslint

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,10 +1,10 @@
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
+  const task = 'I prefer const when I can.';
+  return task;
+}
   
   export function getLast() {
-    return ' is okay';
+  return ' is okay';
   }
   
   export function taskNext() {
