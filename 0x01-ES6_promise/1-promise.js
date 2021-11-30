2363c9aa1ac1d474b2b2de41d22659fff8bfa445
Miguel Pacheco <minipachru@gmail.com>
2021-11-30 13:08:36 -0300

    add task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getFullResponseFromApi(success) {
+  /* eslint-disable */
+  return new Promise((resolve, reject) => {
+    if (false) {
+      throw new Error ("The fake API is not working currently")
+    } else {
+      
+    }
+  })
+}
