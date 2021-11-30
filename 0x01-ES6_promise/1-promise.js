1bbda567b15937098c0c108dcbe4a7fd017e35ad
Miguel Pacheco <minipachru@gmail.com>
2021-11-30 15:34:02 -0300

    fix eslint

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromApi(success) {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (!success) {
       reject(new Error('The fake API is not currently working'));
     } else {
-      resolve({ status: 200, body: 'success'});
+      resolve({ status: 200, body: 'success' });
     }
-  }); 
+  });
 }
