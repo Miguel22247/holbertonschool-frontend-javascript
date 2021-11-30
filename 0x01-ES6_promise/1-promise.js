05b8a02b88c2c545a2ca1b6a64cd148a6a75c1ba
Miguel Pacheco <minipachru@gmail.com>
2021-11-30 13:36:10 -0300

    add files

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,9 +2,9 @@ export default function getFullResponseFromApi(success) {
   /* eslint-disable */
   return new Promise((resolve, reject) => {
     if (success) {
-      return("{ status: 200, body: 'Success' } ");
+      resolve(`{ status: 200, body: 'Success' }`);
     } else {
-      return("The fake API is not working currently");
+      reject("The fake API is not working currently");
     }
   })
 }
---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,6 @@
+export default function divideFunction(numerator, denominator) {
+  try {
+    return numerator / denominator;
+  } catch {
+  }
+}
