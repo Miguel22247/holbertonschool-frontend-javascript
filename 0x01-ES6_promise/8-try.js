5bd5d8d02f9e0e91fa96bc635b886ca6616d2984
Miguel Pacheco <minipachru@gmail.com>
2021-11-30 17:20:48 -0300

    add 8 for testing

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,6 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    return numerator / denominator;
-  } catch {
+  if (denominator != 0 ){
+    return(numerator / denominator);
+  } else {
+    throw new Error("cannot divide by 0")
   }
 }
