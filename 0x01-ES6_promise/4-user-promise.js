ecd05779e5b1631dfb56103bf08020a08e722888
Miguel Pacheco <minipachru@gmail.com>
2021-12-01 10:11:15 -0300

    rewrite code and fix eslint

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,3 +1,7 @@
 export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({ firstName, lastName, });
+  return Promise
+    .resolve({
+      firstName,
+      lastName
+    });
 }
