a3da32ba3db8bb429567b5e8c91d71969d4ca1bf
Miguel Pacheco <minipachru@gmail.com>
2021-12-01 10:57:30 -0300

    add codebase

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,17 @@
 import { signUpUser } from "./4-user-promise";
 import { uploadPhoto } from "./5-photo-reject";
-export default function handleProfileSignup(firstName, lastName, fileName) {
 
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([ signUpUser(firstName, lastName), uploadPhoto(fileName) ])
+    .then((result) => {
+      const profilearray = [];
+      result.forEach((profileindex) => {
+        if (profileindex.status === 'fulfilled') {
+          profilearray.push({ status: profileindex.status, value: profileindex.value, });
+        } else {
+          profilearray.push({ status: profileindex.status, value: `Error: ${profileindex.reason.message}`, });
+        }
+      });
+      return profilearray;
+    });
 }
