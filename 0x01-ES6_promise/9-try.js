b2c7aa2321dcffb12a64984696d4cd23eea92aed
Miguel Pacheco <minipachru@gmail.com>
2021-12-01 11:00:06 -0300

    add codebase of 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,9 +1,10 @@
 export default function guardrail() {
     const queue = [];
+    const string = 'test';
     try {
         const value = mathFunction();
-        queue.push(value, 'Guardrail was processed');
+        queue.push(value, string);
     } catch (e) {
-        queue.push(`Error: ${e.message}`, 'Guardrail was processed');
+        queue.push(`Error: ${e.message}`, string);
     }
 }
