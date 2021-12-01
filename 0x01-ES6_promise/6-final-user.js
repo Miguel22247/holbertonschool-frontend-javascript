867fa448b439976145eea01fa38e7ca88778c4f9
Miguel Pacheco <minipachru@gmail.com>
2021-12-01 13:59:13 -0300

    fix eslint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,15 +3,15 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => {
-    const profilearray = [];
-    result.forEach((index) => {
-      if (index.status !== 'fulfilled') {
-        profilearray.push({ status: index.status, value: `Error: ${index.reason.message}`, });
-      } else {
-        profilearray.push({ status: index.status, value: index.value, });
-      }
+    .then((result) =>{
+      const profilearray = [];
+      result.forEach((index) => {
+        if (index.status !== 'fulfilled') {
+          profilearray.push({ status: index.status, value: `Error: ${index.reason.message}`, });
+        } else {
+          profilearray.push({ status: index.status, value: index.value, });
+        }
+      });
+      return profilearray;
     });
-    return profilearray;
-  });
 }
