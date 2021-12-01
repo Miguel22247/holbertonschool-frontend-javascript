9b54e524e7c6caec7c2283958fac0b14181399c8
Miguel Pacheco <minipachru@gmail.com>
2021-12-01 11:18:40 -0300

    add task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,8 @@
 import { uploadPhoto, createUser } from "./utils.js";
-export default function handleProfileSignup() {
-  const promise1 = Promise.resolve(uploadPhoto);
-  const promise2 = Promise.resolve(createUser);
 
-  return Promise.all([promise1, promise2]).then(values => console.log(values));
+export default function handleProfileSignup() {
+  return Promise.all([ uploadPhoto(), createUser(),])
+  .then((values) => {
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+  }).catch(() => console.log('Signup system offline'));
 }
