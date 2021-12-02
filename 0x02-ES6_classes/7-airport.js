1dea94a6a75dfaff90b62ae4cb8c56fc43687044
Miguel Pacheco <minipachru@gmail.com>
2021-12-02 17:47:19 -0300

    add task 7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,5 +1,9 @@
 export default class Airport {
-    constructor(name, code) {
-        
-    }
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+  toString(){
+    return `[object ${this._code}]`; 
+  }
 }
