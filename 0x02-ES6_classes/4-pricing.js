e825be686134fd9bb965dcf6f541d68e0ede0d20
Miguel Pacheco <minipachru@gmail.com>
2021-12-02 16:13:53 -0300

    add task4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,7 +1,30 @@
 import Currency from './3-currency';
 
 export default class Pricing {
-    constructor(amount, currency) {
+  constructor(amount, currency) {
+      this._currency = currency;
+      this._amount = amount;
+  }
 
-    }
+  // getter
+  get currency() {
+    return this._currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  // setter
+  set currency(Currency) {
+    this._currency = Currency;
+  }
+
+  set amount(Amount) {
+    this._amount = Amount;
+  }
+
+  displayFUllPrice() {
+    return `${this._amount} ${this.currency.displayFUllCurrency()}`;
+  }
 }
