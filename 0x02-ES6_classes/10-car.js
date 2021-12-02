87fddc7a6ce21642155881aaf838ce7f110f81e9
Miguel Pacheco <minipachru@gmail.com>
2021-12-02 18:47:59 -0300

    add 10 for testing

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,7 +1,16 @@
 export default class Car {
-    constructor(brand, motor, color){
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
+  constructor(brand, motor, color){
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
 }
