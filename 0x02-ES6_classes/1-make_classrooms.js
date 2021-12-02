2470d48227764120febee9733b903774365ac77a
Miguel Pacheco <minipachru@gmail.com>
2021-12-02 14:06:37 -0300

    fix eslint

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -5,6 +5,6 @@ export default function initializeRooms() {
   const class1 = new ClassRoom(19);
   const class2 = new ClassRoom(20);
   const class3 = new ClassRoom(34);
-    ClassRoomList.push(class1, class2, class3)
-    return ClassRoomList;
+  ClassRoomList.push(class1, class2, class3)
+  return ClassRoomList;
 }
