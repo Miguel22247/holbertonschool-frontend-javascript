535efb56cb6b22f987c6efa12d6d714fae7f1095
Miguel Pacheco <minipachru@gmail.com>
2021-12-02 17:19:35 -0300

    add 6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,7 +1,22 @@
 import Building from "./5-building";
 
 export default class SkyHighBuilding {
-    constructor(sqft, floors) {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    }
+  // getter
+  get floors() {
+    return this._floors;
+  }
+
+  // setter
+  set floors(Floors) {
+    this._floors = Floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
