ff640161b78b296d53af75858cf600954adbc870
Miguel Pacheco <minipachru@gmail.com>
2021-12-02 18:15:57 -0300

    add task 8

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,5 +1,17 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-        
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    // casting a string
+    if (hint === 'string'){
+      return this._location;
     }
+    // casting a number
+    if (hint === 'number') {
+      return this._size;
+    }
+  }
 }
