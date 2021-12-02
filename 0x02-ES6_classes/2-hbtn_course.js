9eb96dafcf30250a99b397d8ce3ae2cc38c597c6
Miguel Pacheco <minipachru@gmail.com>
2021-12-02 15:15:37 -0300

    add task2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,3 +1,57 @@
 export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw TypeError('Name must be a string');
+    }
 
+    if (typeof length === 'number') {
+      throw TypeError('Length must be a number');
+    } else {
+      this._length = length;
+    }
+
+    if (!Array.isArray(students) && students.every((i) => typeof i !== 'string')) {
+      throw TypeError('Students must be an array of strings');
+    }
+    this._students = students;
+  }
+  // getter
+  get name() {
+    return this._name;
+  }
+
+  // gets the length
+  get length() {
+    return this._length;
+  }
+
+  // gets the amount of students
+  get students() {
+    return this._students;
+  }
+
+  // setter
+  set name(Name) {
+    if (typeof Name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+      this._name = Name;
+  }
+
+  set length(Length) {
+    if (typeof Length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+      this._length = Length;
+  }
+
+  // sets the amount of students
+  set students(Students) {
+    if (!Array.isArray(Students) && Students.every((i) => typeof i !== 'string')) {
+      throw TypeError('Students must be an array of strings');
+    }
+    this._students = Students;
+  }
 }
