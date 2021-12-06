f464507759f09cb6f37fecb5ed39b43f76e0e3a4
Miguel Pacheco <minipachru@gmail.com>
2021-12-06 14:15:26 -0300

    add codebase

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,8 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer, 0);
+
+  if (position > length - 1) {
+    throw Error('Position outside range');
+  }
+}
