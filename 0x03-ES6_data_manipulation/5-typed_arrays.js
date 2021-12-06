9e0ce315d5dc6dfdbbf02abc0fe5869783224df3
Miguel Pacheco <minipachru@gmail.com>
2021-12-06 14:18:04 -0300

    add return and else condition

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,5 +4,8 @@ export default function createInt8TypedArray(length, position, value) {
 
   if (position > length - 1) {
     throw Error('Position outside range');
+  } else {
+    view.setInt8(position, value);
   }
+  return view;
 }
