28716757b3113ca3c4d41081157ce7b99dd62a33
Miguel Pacheco <minipachru@gmail.com>
2021-12-06 10:21:20 -0300

    add task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,20 @@
+export default function getListStudents() {
+  let students = [
+    {
+      "id": "1",
+      "firstName": "Guillaume",
+      "location": "San Francisco"
+    },
+    {
+      "id": "2",
+      "firstName": "Guillaume",
+      "location": "San Francisco"
+    },
+    {
+      "id": "5",
+      "firstName": "Guillaume",
+      "location": "San Francisco"
+    },
+  ]
+  console.log(students);
+}
