1cd2262ea772baee65eeaedb2fff83cafb99ac6e
Miguel Pacheco <minipachru@gmail.com>
2021-12-06 12:39:22 -0300

    fix eslint

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,19 @@
 export default function getListStudents() {
   let students = [
     {
-      "id": "1",
-      "firstName": "Guillaume",
-      "location": "San Francisco"
+      "id": '1',
+      "firstName": 'Guillaume',
+      "location": 'San Francisco'
     },
     {
-      "id": "2",
-      "firstName": "James",
-      "location": "Columbia"
+      "id": '2',
+      "firstName": 'James',
+      "location": 'Columbia'
     },
     {
-      "id": "5",
-      "firstName": "Serena",
-      "location": "San Francisco"
+      "id": '5',
+      "firstName": 'Serena',
+      "location": 'San Francisco'
     },
   ]
   console.log(students);
