d1c280fe153d3aabd14a2eb654e41cd3d7102fdc
Miguel Pacheco <minipachru@gmail.com>
2021-12-06 14:03:33 -0300

    add final code

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,13 @@
-export default function updateStudentGradeByCity(array, city, grade) {
+export default function updateStudentGradeByCity(array, city, grad) {
   return array
+  // filters by localization
   .filter((item) => item.location === city)
   .map((student) => {
-    const gradeItems = grade
+    //filters by student id and maps by grade
+    const gradeItems = grad
     .filter((item) => item.studentId === student.id)
-  })
+    .map((n) => n.grade)[0];
+    const grade = gradeItems || 'N/A';
+    return { ...student, grade};
+  });
 }
