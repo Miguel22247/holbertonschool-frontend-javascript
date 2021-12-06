ea2074220db33f730ce001530881dbb240455a50
Miguel Pacheco <minipachru@gmail.com>
2021-12-06 13:52:24 -0300

    add first version of mapper

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,4 +1,8 @@
-export default function updateStudentGradeByCity(array, city) {
+export default function updateStudentGradeByCity(array, city, grade) {
   return array
   .filter((item) => item.location === city)
+  .map((student) => {
+    const gradeItems = grade
+    .filter((item) => item.studentId === student.id)
+  })
 }
