3d77d834ddcb7a36a9d05e099b260bebf4f0c037
Miguel Pacheco <minipachru@gmail.com>
2021-12-06 14:54:38 -0300

    rewrited code

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,7 @@
 export default function getListStudentIds(studentIdArray) {
-  if (!Array.isArray(studentIdArray)) return [];
-  return studentIdArray.map((item) => item.id);
+  if (!Array.isArray(studentIdArray)){
+    return [];
+  } else {
+    return studentIdArray.map((item) => item.id);
+  }
 }
