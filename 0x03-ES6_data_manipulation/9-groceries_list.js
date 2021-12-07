43a0494d885fee5bc1f8fd45f993e92d87e99503
Miguel Pacheco <minipachru@gmail.com>
2021-12-07 09:25:02 -0300

    fix set of elements in line 5

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -2,7 +2,7 @@ export default function groceriesList() {
   const groceriesArray = [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]];
   const groceriesMap = new Map();
   for (const elements of groceriesArray) {
-    groceryMap.set(elements[0], elements[1]);
+    groceriesMap.set(elements[0], elements[1]);
   }
   return groceriesMap;
 }
