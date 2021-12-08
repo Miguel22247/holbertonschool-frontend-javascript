019cde4eca0810b7d6b86aaf8692e8bceac00d64
Miguel Pacheco <minipachru@gmail.com>
2021-12-08 12:18:53 -0300

    add files

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,40 @@
+
+
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const studentA : Student = {
+  firstName: 'Miguel',
+  lastName: 'Pacheco',
+  age: 19,
+  location: 'Uruguay',
+}
+
+const studentB : Student = {
+  firstName: 'Javier',
+  lastName: 'Valenzani',
+  age: 28,
+  location: 'Earth',
+}
+
+const studentList = [studentA, studentB];
+
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+
+studentList.forEach((obj) => {
+  const row = document.createElement('tr');
+  const cellName = document.createElement('td');
+  const cellLocation = document.createElement('td');
+  cellName.textContent = obj.firstName;
+  cellLocation.textContent = obj.location;
+  row.appendChild(cellName);
+  row.appendChild(cellLocation);
+  tbody.appendChild(row);
+});
+table.appendChild(tbody);
+document.body.appendChild(table);
