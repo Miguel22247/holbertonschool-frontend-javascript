96927f0e65915979beaaed92a5983bd5b63808d2
Miguel Pacheco <minipachru@gmail.com>
2021-12-08 21:08:30 -0300

    add task 5

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,9 +1,22 @@
 interface MinorCredits {
   brand: 'minor';
-  credits: number;
+  credit: number;
 }
 
 interface MajorCredits {
   brand: 'major';
-  credits: number;
+  credit: number;
+}
+
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  const sum = subject1.credit + subject2.credit;
+  const obj: MajorCredits = { credit: sum, brand: "major", };
+  return obj;
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  const sum = subject1.credit + subject2.credit;
+  const obj: MinorCredits = { credit: sum, brand: "minor", };
+  return obj;
 }
